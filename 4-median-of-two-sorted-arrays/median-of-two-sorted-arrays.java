12668fda3d8cccafa5484d067389e2e7ebbfffb2
Mahesh <107562051+Maheshh-Busam@users.noreply.github.com>
2026-06-12 13:31:37 +0530

    Time: 1 ms (100.00%) | Memory: 48.9 MB (50.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,21 +3,31 @@ class Solution {
         int n = nums1.length;
         int m = nums2.length;
 
-        double median = 0;
+        int[] merged = new int[n + m];
 
-        int[] merged = new int[n+m];
+        int p1 = 0, p2 = 0;
+        int index = 0;
 
-        for(int i = 0; i < n; i++){
-            merged[i] = nums1[i];
+        while(p1 < n && p2 < m){
+            if(nums1[p1] < nums2[p2]){
+                merged[index++] = nums1[p1++]; 
+            }
+            else {
+                merged[index++] = nums2[p2++];
+            }
         }
 
-        for(int i = 0; i < m; i++){
-            merged[n + i] = nums2[i];
+        while(p1 < n){
+            merged[index++] = nums1[p1++];
         }
 
-        Arrays.sort(merged);
+        while(p2 < m){
+            merged[index++] = nums2[p2++];
+        }
 
         int mergedLength = merged.length;
+
+        double median = 0;
 
         if(mergedLength % 2 == 0){
             median = (merged[mergedLength/2] + merged[mergedLength/2 - 1]) / 2.0;
