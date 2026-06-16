3c0a7427801bda44aa8046ad98272e37f5196506
Mahesh <107562051+Maheshh-Busam@users.noreply.github.com>
2026-06-16 20:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (49.60%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -9,21 +9,11 @@ class Solution {
                 return mid;
             }
 
-            if(nums[left] <= nums[mid]){
-                if(nums[left] <= target && target <= nums[mid]){
-                    right = mid - 1;
-                }
-                else{
-                    left = mid + 1;
-                }
+            if(nums[mid] < target){
+                left = mid + 1;
             }
             else {
-                if(nums[mid] <= target && target <= nums[right]){
-                    left =mid + 1;
-                }
-                else {
-                    right = mid - 1;
-                }
+                right = mid - 1;
             }
         }
         return -1;
