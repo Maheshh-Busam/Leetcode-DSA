65579e4f138dca511897fc9fd02d0a7470cb0c27
Mahesh <107562051+Maheshh-Busam@users.noreply.github.com>
2026-06-18 20:52:30 +0530

    File updated at 6/18/2026, 8:52:30 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,24 +1,40 @@
-// Last updated: 6/18/2026, 8:47:17 PM
+// Last updated: 6/18/2026, 8:52:30 PM
 1class Solution {
 2    public int[] searchRange(int[] nums, int target) {
-3        int left = 0, right = nums.length - 1;
+3        int left1 = 0, right1 = nums.length - 1;
 4        int[] result = new int[]{-1,-1};
 5
-6        while(left  < nums.length){
-7            if(nums[left] == target){
-8                result[0] = left;
-9                break;
-10            }
-11            left++;
-12        }
-13
-14        while(right >= 0) {
-15            if(nums[right] == target){
-16                result[1] = right;
-17                break;
+6        while(left1 <= right1){
+7            int mid = (left1 + right1) / 2;
+8
+9            if(nums[mid] == target) {
+10                result[0] = mid;
+11                right1 = mid - 1;
+12            }
+13            else if(nums[mid] < target){
+14                left1 = mid + 1;
+15            }
+16            else {
+17                right1 = mid - 1;
 18            }
-19            right--;
-20        }
-21        return result;
-22    }
-23}
+19        }
+20
+21        int left2 = 0, right2 = nums.length - 1;
+22
+23        while(left2 <= right2){
+24            int mid = (left2 + right2)/2;
+25
+26            if(nums[mid] == target){
+27                result[1] = mid;
+28                left2 = mid + 1;
+29            }
+30            else if(nums[mid] < target){
+31                left2 = mid + 1;
+32            }
+33            else {
+34                right2 = mid - 1;
+35            }
+36        }
+37        return result;
+38    }
+39}
