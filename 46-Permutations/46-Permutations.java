4c40643f5a2b5d2f65e4a1f6edd27383f937ae1b
Mahesh <107562051+Maheshh-Busam@users.noreply.github.com>
2026-06-22 11:05:51 +0530

    File updated at 6/22/2026, 11:05:51 AM

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,34 +1,33 @@
-// Last updated: 6/20/2026, 9:01:54 PM
+// Last updated: 6/22/2026, 11:05:51 AM
 1class Solution {
 2    public List<List<Integer>> permute(int[] nums) {
-3        List<List<Integer>> result = new ArrayList<>();
+3        List<List<Integer>> result =new ArrayList<>();
 4        List<Integer> current = new ArrayList<>();
 5
 6        permutations(nums, result, current);
 7
 8        return result;
-9
-10    }
-11
-12    public void permutations(int[] nums, List<List<Integer>> result, List<Integer> current){
-13
-14        if(nums.length == current.size()){
-15            List<Integer> permutations = new ArrayList<>(current);
-16            result.add(permutations);
-17            return;
-18        }
-19
-20        for(int i = 0; i < nums.length; i++) {
-21
-22            if(current.contains(nums[i])){
-23                continue;
-24            }
-25
-26            current.add(nums[i]);
-27
-28            permutations(nums, result, current);
-29
-30            current.remove(current.size() - 1);
-31        }
-32    }
-33}
+9    }
+10
+11    public void permutations(int[] nums, List<List<Integer>> result, List<Integer> current) {
+12
+13        if(nums.length == current.size()){
+14            List<Integer> permutations = new ArrayList<>(current);
+15            result.add(permutations);
+16            return;
+17        }
+18
+19        for(int i = 0; i < nums.length; i++){
+20
+21            if(current.contains(nums[i])){
+22                continue;
+23            }
+24
+25            current.add(nums[i]);
+26
+27            permutations(nums, result, current);
+28
+29            current.remove(current.size() - 1);
+30        }
+31    }
+32}
