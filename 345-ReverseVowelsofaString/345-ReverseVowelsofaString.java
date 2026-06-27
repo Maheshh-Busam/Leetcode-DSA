1e1b85da58fab386d77799df2ce4bfc33ae607d6
Mahesh <107562051+Maheshh-Busam@users.noreply.github.com>
2026-06-27 14:16:54 +0530

    File updated at 6/27/2026, 2:16:53 PM

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
@@ -1,4 +1,4 @@
-// Last updated: 6/27/2026, 2:10:49 PM
+// Last updated: 6/27/2026, 2:16:53 PM
 1class Solution {
 2    public String reverseVowels(String s) {
 3        Set<Character> vowelsSet = new HashSet<>();
@@ -17,27 +17,24 @@
 16
 17        int left = 0, right = charArray.length - 1;
 18
-19        while(left <= right){
+19        while (left <= right) {
 20
-21            if(vowelsSet.contains(charArray[left]) && vowelsSet.contains(charArray[right])) {
-22                char temp = charArray[left];
-23                charArray[left] = charArray[right];
-24                charArray[right] = temp;
-25
-26                left++;
-27                right--;
-28                
-29            }
-30            else if(!vowelsSet.contains(charArray[left])){
-31                left++;
-32            }
-33            else if(!vowelsSet.contains(charArray[right])){
-34                right--;
-35            }
-36        }
-37
-38        System.out.println(new String(charArray));
-39
-40        return new String(charArray);
-41    }
-42}
+21            while (left < right && !vowelsSet.contains(charArray[left])) {
+22                left++;
+23            }
+24            while (left < right && !vowelsSet.contains(charArray[right])) {
+25                right--;
+26            }
+27
+28            char temp = charArray[left];
+29            charArray[left] = charArray[right];
+30            charArray[right] = temp;
+31
+32            left++;
+33            right--;
+34
+35        }
+36
+37        return new String(charArray);
+38    }
+39}
