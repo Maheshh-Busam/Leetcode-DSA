e088aac7d40735f0de9f1c52a5ec237744590e18
Mahesh <107562051+Maheshh-Busam@users.noreply.github.com>
2026-06-27 22:16:12 +0530

    File updated at 6/27/2026, 10:16:12 PM

--- 392-IsSubsequence/392-IsSubsequence.java
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -1,26 +1,23 @@
-// Last updated: 6/27/2026, 10:13:32 PM
+// Last updated: 6/27/2026, 10:16:12 PM
 1class Solution {
 2    public boolean isSubsequence(String s, String t) {
 3        if(s.length() > t.length()){
 4            return false;
 5        }
 6
-7        int start = 0;
+7        int p1 = 0, p2 = 0;
 8
-9        for(int i = 0; i < s.length(); i++){
-10            boolean found = false;
-11
-12            for(int j = start; j < t.length(); j++){
-13                if(s.charAt(i) == t.charAt(j)){
-14                    found = true;
-15                    start = j + 1;
-16                    break;
-17                }
-18            }
-19            if(!found){
-20                return false;
-21            }
-22        }
-23        return true;
-24    }
-25}
+9        while(p1 < s.length() && p2 < t.length()){
+10
+11            if(s.charAt(p1) == t.charAt(p2)){
+12                p1++;
+13            }
+14            p2++;
+15        }
+16
+17        if(p1 != s.length()){
+18            return false;
+19        }
+20        return true;
+21    }
+22}
