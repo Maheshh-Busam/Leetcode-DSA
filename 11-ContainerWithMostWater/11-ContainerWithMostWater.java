def14f2d4c85ae4bac44e546fe24c8c18a0c1db0
Mahesh <107562051+Maheshh-Busam@users.noreply.github.com>
2026-06-27 09:59:45 +0530

    File updated at 6/27/2026, 9:59:45 AM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,20 +1,24 @@
-// Last updated: 6/26/2026, 3:14:48 PM
+// Last updated: 6/27/2026, 9:59:45 AM
 1class Solution {
 2    public int maxArea(int[] height) {
-3        int left = 0, right = height.length - 1;
-4        int containerWithMostWater = 0;
+3        int containerWithMostWater = 0;
+4        int left = 0, right = height.length - 1;
 5
-6        while (left <= right) {
-7            int width = right - left;
-8            int currContainer = width * Math.min(height[left], height[right]);
-9            containerWithMostWater = Math.max(currContainer, containerWithMostWater);
+6        while(left <= right){
+7
+8            int width = right - left;
+9            int currentContainer = width * Math.min(height[left], height[right]);
 10
-11            if (height[left] < height[right]) {
-12                left++;
-13            } else {
-14                right--;
+11            containerWithMostWater = Math.max(currentContainer, containerWithMostWater);
+12
+13            if(height[left] < height[right]){
+14                left++;
 15            }
-16        }
-17        return containerWithMostWater;
-18    }
-19}
+16            else {
+17                right--;
+18            }
+19        }
+20
+21        return containerWithMostWater;
+22    }
+23}
