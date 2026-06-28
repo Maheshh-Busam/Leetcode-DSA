1a7f38c6cf76bf5f8c5b3c56fba2d715626a9f1a
Mahesh <107562051+Maheshh-Busam@users.noreply.github.com>
2026-06-28 11:09:39 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/version/java/125-ValidPalindrome_20260628_110939.java
@@ -0,0 +1,28 @@
+// Last updated: 6/28/2026, 11:09:39 AM
+1class Solution {
+2    public boolean isPalindrome(String s) {
+3        s = s.toLowerCase();
+4
+5        int left = 0, right = s.length() - 1;
+6
+7        while(left <= right){
+8            
+9             while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+10                left++;
+11            }
+12            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+13                right--;
+14            }
+15
+16            if(s.charAt(left) != s.charAt(right)){
+17                return false;
+18            }
+19
+20            left++;
+21            right--;
+22        }
+23        
+24        return true;
+25    }
+26}
+27
